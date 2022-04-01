f95eddd7e79b17dfb388678b3c3ea4c89ca9b7d9
jokoho482 <hoffmann.jonas95@gmail.com>
2022-04-01 20:40:36 +0200

    some TOML fixes

---
+++ mod.cpp
@@ -0,0 +1,15 @@
+name = "Hearking Indicator {version}";
+dir = "@JK_HearingIndicator";
+author = "joko // Jonas";
+
+tooltipOwned = "Joko's Hearing Indicator";
+description = "Joko's Hearing Indicator";
+picture = "";
+logoSmall = "";
+logo = "";
+logoOver = "";
+hidePicture = 0;
+hideName = 0;
+actionName = "";
+action = "";
+overview = "";
